961cb0952acf6b0931b0de90921514877f87a355
MakotoInomata <inomata@futurewoods.co.jp>
2018-08-06 18:21:52 +0900

    fix posts

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title><?php bzb_title(); ?></title>
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <?php echo get_option('analytics_tracking_code');?>
   <?php 
   wp_head();
